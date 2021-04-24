119aad37ab64adcf3b9ff494fd890af61e96fcd8
ThiagoJA <thiago_jesuino@hotmail.com>
2021-04-24 00:21:46 -0300

    Fix logic problem in caesarCipher

--- src/helpers/Encrypts.js
+++ src/helpers/Encrypts.js
@@ -1,7 +1,8 @@
 const caesarCipher = (text, setEncryptedText) => {
 	let auxText = "";
 	for (let i = 0; i <= text.length - 1; i++) {
-		const encryptedCode = text.charCodeAt(i) >= 120 ? 122 - text.charCodeAt(i) + 97 : text.charCodeAt(i) + 3
+		console.log(text.charCodeAt(i))
+		const encryptedCode = text.charCodeAt(i) >= 120 ? (text.charCodeAt(i) - 120)*1 + 97 : text.charCodeAt(i) + 3
 		auxText += String.fromCharCode(encryptedCode)
 	}
 	setEncryptedText(auxText)
